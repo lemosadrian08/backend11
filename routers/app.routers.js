9d64a2482ae69f1905a2681cd989a75500402d74
Adrian Lemos <lemosadrian08@gmail.com>
2022-12-04 20:57:50 -0300

    first commit

---
+++ routers/app.routers.js
@@ -0,0 +1,15 @@
+const { Router } = require("express");
+const productsRoutes = require('./products/products.routes');
+
+
+
+const router = Router();
+
+
+router.use('/products', productsRoutes)
+
+
+
+
+
+module.exports = router;
